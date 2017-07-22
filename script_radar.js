47c8bb8503a5588a864ec32433e17036b57eceae
ammii <ammii@AMMII-MBP.local>
2017-07-23 03:16:20 +0700

    add new graph

--- script_radar.js
+++ script_radar.js
@@ -1,122 +1,93 @@
+(function() {
+  angular.module("RadarChart", [])
+    .directive("radar", radar)
+    .directive("onReadFile", onReadFile)
+    .controller("MainCtrl", MainCtrl);
 
-var w = 500,
-	h = 500;
+  // controller function MainCtrl
+  function MainCtrl($http) {
+    var ctrl = this;
+    init();
 
-var colorscale = d3.scale.category10();
 
-//Legend titles
-var LegendOptions = ['Smartphone','Tablet'];
+    // function init
+    function init() {
+      // initialize controller variables
+      ctrl.examples = [
+        "data_the_avengers",
+        "data_plant_seasons",
+        "data_car_ratings"
+      ];
+      ctrl.exampleSelected = ctrl.examples[0];
+      ctrl.getData = getData;
+      ctrl.selectExample = selectExample;
 
-//Data
-var d = [
-		  [
-			{axis:"Email",value:0.59},
-			{axis:"Social Networks",value:0.56},
-			{axis:"Internet Banking",value:0.42},
-			{axis:"News Sportsites",value:0.34},
-			{axis:"Search Engine",value:0.48},
-			{axis:"View Shopping sites",value:0.14},
-			{axis:"Paying Online",value:0.11},
-			{axis:"Buy Online",value:0.05},
-			{axis:"Stream Music",value:0.07},
-			{axis:"Online Gaming",value:0.12},
-			{axis:"Navigation",value:0.27},
-			{axis:"App connected to TV program",value:0.03},
-			{axis:"Offline Gaming",value:0.12},
-			{axis:"Photo Video",value:0.4},
-			{axis:"Reading",value:0.03},
-			{axis:"Listen Music",value:0.22},
-			{axis:"Watch TV",value:0.03},
-			{axis:"TV Movies Streaming",value:0.03},
-			{axis:"Listen Radio",value:0.07},
-			{axis:"Sending Money",value:0.18},
-			{axis:"Other",value:0.07},
-			{axis:"Use less Once week",value:0.08}
-		  ],[
-			{axis:"Email",value:0.48},
-			{axis:"Social Networks",value:0.41},
-			{axis:"Internet Banking",value:0.27},
-			{axis:"News Sportsites",value:0.28},
-			{axis:"Search Engine",value:0.46},
-			{axis:"View Shopping sites",value:0.29},
-			{axis:"Paying Online",value:0.11},
-			{axis:"Buy Online",value:0.14},
-			{axis:"Stream Music",value:0.05},
-			{axis:"Online Gaming",value:0.19},
-			{axis:"Navigation",value:0.14},
-			{axis:"App connected to TV program",value:0.06},
-			{axis:"Offline Gaming",value:0.24},
-			{axis:"Photo Video",value:0.17},
-			{axis:"Reading",value:0.15},
-			{axis:"Listen Music",value:0.12},
-			{axis:"Watch TV",value:0.1},
-			{axis:"TV Movies Streaming",value:0.14},
-			{axis:"Listen Radio",value:0.06},
-			{axis:"Sending Money",value:0.16},
-			{axis:"Other",value:0.07},
-			{axis:"Use less Once week",value:0.17}
-		  ]
-		];
+      // initialize controller functions
+      ctrl.selectExample(ctrl.exampleSelected);
+      ctrl.config = {
+        w: 250,
+        h: 250,
+        facet: false,
+        levels: 5,
+        levelScale: 0.85,
+        labelScale: 0.9,
+        facetPaddingScale: 2.1,
+        showLevels: true,
+        showLevelsLabels: false,
+        showAxesLabels: true,
+        showAxes: true,
+        showLegend: true,
+        showVertices: true,
+        showPolygons: true
+      };
+    }
 
-//Options for the Radar chart, other than default
-var mycfg = {
-  w: w,
-  h: h,
-  maxValue: 0.6,
-  levels: 6,
-  ExtraWidthX: 300
-}
+    // function getData
+    function getData($fileContent) {
+      ctrl.csv = $fileContent;
+    }
 
-//Call function to draw the Radar chart
-//Will expect that data is in %'s
-RadarChart.draw("#chart", d, mycfg);
+    // function selectExample
+    function selectExample(item) {
+      var file = item + ".csv";
+      $http.get(file).success(function(data) {
+        ctrl.csv = data;
+      });
+    }
+  }
 
-////////////////////////////////////////////
-/////////// Initiate legend ////////////////
-////////////////////////////////////////////
+  // directive function sunburst
+  function radar() {
+    return {
+      restrict: "E",
+      scope: {
+        csv: "=",
+        config: "="
+      },
+      link: radarDraw
+    };
+  }
 
-var svg = d3.select('#body')
-	.selectAll('svg')
-	.append('svg')
-	.attr("width", w+300)
-	.attr("height", h)
 
-//Create the title for the legend
-var text = svg.append("text")
-	.attr("class", "title")
-	.attr('transform', 'translate(90,0)')
-	.attr("x", w - 70)
-	.attr("y", 10)
-	.attr("font-size", "12px")
-	.attr("fill", "#404040")
-	.text("What % of owners use a specific service in a week");
-
-//Initiate Legend
-var legend = svg.append("g")
-	.attr("class", "legend")
-	.attr("height", 100)
-	.attr("width", 200)
-	.attr('transform', 'translate(90,20)')
-	;
-	//Create colour squares
-	legend.selectAll('rect')
-	  .data(LegendOptions)
-	  .enter()
-	  .append("rect")
-	  .attr("x", w - 65)
-	  .attr("y", function(d, i){ return i * 20;})
-	  .attr("width", 10)
-	  .attr("height", 10)
-	  .style("fill", function(d, i){ return colorscale(i);})
-	  ;
-	//Create text next to squares
-	legend.selectAll('text')
-	  .data(LegendOptions)
-	  .enter()
-	  .append("text")
-	  .attr("x", w - 52)
-	  .attr("y", function(d, i){ return i * 20 + 9;})
-	  .attr("font-size", "11px")
-	  .attr("fill", "#737373")
-	  .text(function(d) { return d; })
-	  ;
+  // directive function onReadFile
+  function onReadFile($parse) {
+    return {
+      restrict: "A",
+      scope: false,
+      link: function(scope, element, attrs) {
+        var fn = $parse(attrs.onReadFile);
+        element.on("change", function(onChangeEvent) {
+          var reader = new FileReader();
+          reader.onload = function(onLoadEvent) {
+            scope.$apply(function() {
+              fn(scope, {
+                $fileContent: onLoadEvent.target.result
+              });
+            });
+          };
+          reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
+        });
+      }
+    };
+  }
+})();
